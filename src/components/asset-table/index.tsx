474a700569c4936609497789fd7b586c306d2ebf
onuriltan <onur.iltan@gmail.com>
2022-06-22 12:38:49 +0300

    chore: type support for rendering assets

--- src/components/asset-table/index.tsx
+++ src/components/asset-table/index.tsx
@@ -2,8 +2,9 @@ import "./index.scss";
 
 import classnames from "classnames";
 import { arrangeCexName, capitalizeFirstLetter, toUsd, isValidHttpUrl, platformInfo } from "@utils/index";
+import { CexAsset, DexAsset } from "@customTypes/index";
 
-export const Index = ({ assets, loading }: { assets: any; loading: boolean }) => {
+export const Index = ({ assets, loading }: { assets: (DexAsset | CexAsset)[]; loading: boolean }) => {
   const openAssetAtScan = (scanUrl: string) => {
     if (scanUrl && isValidHttpUrl(scanUrl)) {
       window.open(scanUrl, "_blank");
@@ -28,97 +29,90 @@ export const Index = ({ assets, loading }: { assets: any; loading: boolean }) =>
           </div>
         )}
         {assets &&
-          assets.map(
-            (
-              {
-                name,
-                symbol,
-                price,
-                balance,
-                value,
-                platform,
-                logo,
-                walletName,
-                cexName,
-                scan,
-              }: {
-                name: string;
-                symbol: any;
-                price: any;
-                balance: any;
-                value: any;
-                platform: any;
-                logo: any;
-                walletName: any;
-                cexName: any;
-                scan: any;
-              },
-              index: number,
-            ) => {
-              let platformImage = platformInfo.filter((info) => info.name === (cexName || platform)) as any;
-              if (platformImage.length === 1) {
-                platformImage = platformInfo.filter((info) => info.name === (cexName || platform))[0].image;
-              } else {
-                platformImage = undefined;
-              }
-              if (!symbol) {
-                return <div key={name + index}></div>;
-              }
-
-              return (
+          assets.map((asset, index: number) => {
+            let platformImage = "";
+            let symbol = "";
+            let scan = "";
+            let logo = "";
+            const cexName = "";
+            let platform = "";
+            let balance = 0;
+            let walletName = "";
+            let price = 0;
+            let value = 0;
+            if ("platform" in asset) {
+              platformImage = platformInfo.filter((info) => info.name === asset.platform)[0]?.image;
+              symbol = asset.symbol;
+              scan = asset.scan;
+              logo = asset.logo;
+              platform = asset.platform;
+              price = asset.price;
+              balance = asset.balance;
+              walletName = asset.walletName;
+              value = asset.value;
+            }
+            if ("cexName" in asset) {
+              platformImage = platformInfo.filter((info) => info.name === asset.cexName)[0]?.image;
+              symbol = asset.symbol;
+              logo = asset.logo;
+              price = asset.price;
+              balance = asset.balance;
+              value = asset.value;
+            }
+            if (!symbol) {
+              return <div key={symbol + index}></div>;
+            }
+            return (
+              <div
+                key={symbol + index}
+                className={classnames(
+                  "asset-table__assets__asset",
+                  index === 0 && "asset-table__assets__asset--first",
+                  assets.length - 1 && "asset-table__assets__asset--last",
+                )}
+                onClick={() => openAssetAtScan(scan)}
+              >
                 <div
-                  key={name + index}
-                  className={classnames(
-                    "asset-table__assets__asset",
-                    index === 0 && "asset-table__assets__asset--first",
-                    assets.length - 1 && "asset-table__assets__asset--last",
-                  )}
-                  onClick={() => openAssetAtScan(scan)}
-                >
-                  <div
-                    className="asset-table__assets__asset__item 
+                  className="asset-table__assets__asset__item 
                                 asset-table__assets__asset__item--asset 
                                 asset-table__assets__asset__item--first"
-                  >
-                    {logo && <img src={logo} alt={symbol} className="asset-table__assets__asset__item__image" />}
+                >
+                  {logo && <img src={logo} alt={symbol} className="asset-table__assets__asset__item__image" />}
 
-                    {!logo && (
-                      <div className="asset-table__assets__asset__item__noimage">
-                        {symbol.toUpperCase().slice(0, 3)}
-                      </div>
-                    )}
-                    <div className="asset-table__assets__asset__item__chain">
-                      <div className="asset-table__assets__asset__item__chain__name">{symbol.toUpperCase()}</div>
-                      <div className="asset-table__assets__asset__item__chain__area">
-                        {!platformImage && (
-                          <div className="asset-table__assets__asset__item__noimage">
-                            {symbol.toUpperCase().slice(0, 3)}
-                          </div>
-                        )}
-                        {platformImage && (
-                          <img
-                            src={platformImage}
-                            alt={cexName || platform}
-                            className="asset-table__assets__asset__item__chain__logo"
-                          />
-                        )}
-                        {arrangeCexName(cexName)}
-                        {capitalizeFirstLetter(platform)}
-                      </div>
+                  {!logo && (
+                    <div className="asset-table__assets__asset__item__noimage">{symbol.toUpperCase().slice(0, 3)}</div>
+                  )}
+                  <div className="asset-table__assets__asset__item__chain">
+                    <div className="asset-table__assets__asset__item__chain__name">{symbol.toUpperCase()}</div>
+                    <div className="asset-table__assets__asset__item__chain__area">
+                      {!platformImage && (
+                        <div className="asset-table__assets__asset__item__noimage">
+                          {symbol.toUpperCase().slice(0, 3)}
+                        </div>
+                      )}
+                      {platformImage && (
+                        <img
+                          src={platformImage}
+                          alt={cexName || platform}
+                          className="asset-table__assets__asset__item__chain__logo"
+                        />
+                      )}
+                      {arrangeCexName(cexName)}
+                      {capitalizeFirstLetter(platform)}
                     </div>
                   </div>
-                  <div className="asset-table__assets__asset__item ">{toUsd(price)}</div>
-                  <div className="asset-table__assets__asset__item">{balance}</div>
-                  <div className="asset-table__assets__asset__item asset-table__assets__asset__item--center">
-                    {walletName}
-                  </div>
-                  <div className="asset-table__assets__asset__item asset-table__assets__asset__item--last">
-                    {toUsd(value)}
-                  </div>
                 </div>
-              );
-            },
-          )}
+                <div className="asset-table__assets__asset__item ">{toUsd(price)}</div>
+                <div className="asset-table__assets__asset__item">{balance}</div>
+                <div className="asset-table__assets__asset__item asset-table__assets__asset__item--center">
+                  {walletName}
+                </div>
+                <div className="asset-table__assets__asset__item asset-table__assets__asset__item--last">
+                  {toUsd(value)}
+                </div>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
