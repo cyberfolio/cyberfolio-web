29256a73bf811794cf41c3715c9c674f8d670ca6
Onur Iltan <onur.iltan@gmail.com>
2022-07-01 10:57:00 +0300

    feat: but me a coffee (#7)
    feat: final

--- src/config/links.ts
+++ src/config/links.ts
@@ -2,6 +2,7 @@ const links = {
   twitter: "https://twitter.com/onur_iltan",
   linkedin: "https://www.linkedin.com/in/onur-iltan/",
   github: "https://github.com/cyberfolio",
+  buyMeACoffee: "https://www.buymeacoffee.com/onuriltan",
 };
 
 export default links;
